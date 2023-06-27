13f1578ba7a24bc77b1852dcb15cc7c98d42d629
Chaz Coats <chazcoats2@hotmail.com>
2023-06-27 12:42:23 -0400

    Working out the quiz display JS code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,14 @@
 var button = document.getElementById("#startBtn");
+var quiz = document.getElementById("#quiz");
 var timerDisplay = document.getElementById("#timer");
-var questionBox = document.getElementById("#questions");
-var answerEl = document.getElementsByClassName(".answer");
+var questionEl = document.getElementById("#questions");
+var answerEl = document.getElementsByClassName("#answers");
 var answerA = document.getElementById("#answer-a");
 var answerB = document.getElementById("#answer-b");
 var answerC = document.getElementById("#answer-c");
 var answerD = document.getElementById("#answer-d");
 var scoreBox = document.getElementById("#score");
+var contBtn = document.getElementById("#contBtn");
 var initTime = 60;
 var score = 0;
 var questionNumber = 0;
@@ -63,29 +65,26 @@ var questions = [
     rightanswer: ""
   },
 ]
-//deselects check boxes next to answers
-function deselector() {
-answerEl.array.forEach((answer) => (answer.checked = false));
-}
-
-function userAnswer() {
-  var answer;
-  answerEl.forEach(answerElement) {
-    if (answerElement.checked) {
-      answer = answerElement.id;
-    };
-    return answer;
-  };
-};
 
 function questionDisplay() {
   deselector();
-  var testQuestions = questions[questionNumber];
-  questionBox.innerHTML = testQuestions.question;
-  answerA.innerHTML = testQuestions.A;
-  answerB.innerHTML = testQuestions.B;
-  answerC.innerHTML = testQuestions.C;
-  answerD.innerHTML = testQuestions.D;
+questionEl.textContent = questions[questionNumber].question;
+answerEl.innerHTML= ""
+for (let i=0; i < questions[questionNumber].A.length; i++) {
+  var answersDiv = document.createElement("div");
+  var answerlist = document.createElement("input");
+  var answerLabel = document.createElement("label");
+
+  answerlist.type = "radio";
+  answerlist.name = "answer";
+  answerlist.value = i;
+
+  answerLabel.textContent = questions[questionNumber].A[i].text;
+
+  answersDiv.appendChild(answerlist);
+  answersDiv.appendChild(answerLabel);
+  answerEl.appendChild(answersDiv);
+  }
 }
 
 questionDisplay();
@@ -97,20 +96,108 @@ function timer() {
   timerDisplay.textContent = initTime;
    }, 1000);
 }
-  timer();
 
 button.addEventListener("click", function() {
-  var answer = userAnswer();
+  button.style.display = "none";
+  contBtn.style.display = "inline";
+  const answer = userAnswer();
   if (answer) {
-    if (answer === questions[questionNumber].rightanswer) {
-      score++;
+    if (answer === questions[questionNumber].rightanswer) score++;
       questions++;
-    }
-    if (questionNumber < questions.length) {
-      questionDisplay();
-    } else {
-      
-    }
+    
+    if (questionNumber < questions.length) questionDisplay();
+      else {
+        quiz.innerHTML =
+      }
+    
     
   }
 })
+const Questions = [{
+  q: "What is capital of India?",
+  a: [{ text: "Gandhinagar", isCorrect: false },
+  { text: "Surat", isCorrect: false },
+  { text: "Delhi", isCorrect: true },
+  { text: "Mumbai", isCorrect: false }
+  ]
+
+},
+{
+  q: "What is the capital of Thailand?",
+  a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+  { text: "Phuket", isCorrect: false },
+  { text: "Ayutthaya", isCorrect: false },
+  { text: "Bangkok", isCorrect: true }
+  ]
+
+},
+{
+  q: "What is the capital of Gujarat",
+  a: [{ text: "Surat", isCorrect: false },
+  { text: "Vadodara", isCorrect: false },
+  { text: "Gandhinagar", isCorrect: true },
+  { text: "Rajkot", isCorrect: false }
+  ]
+
+}
+
+]
+
+let currQuestion = 0
+let score = 0
+
+function loadQues() {
+  const question = document.getElementById("ques")
+  const opt = document.getElementById("opt")
+
+  question.textContent = Questions[currQuestion].q;
+  opt.innerHTML = ""
+
+  for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+      const choicesdiv = document.createElement("div");
+      const choice = document.createElement("input");
+      const choiceLabel = document.createElement("label");
+
+      choice.type = "radio";
+      choice.name = "answer";
+      choice.value = i;
+
+      choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+      choicesdiv.appendChild(choice);
+      choicesdiv.appendChild(choiceLabel);
+      opt.appendChild(choicesdiv);
+  }
+}
+
+loadQues();
+
+function loadScore() {
+  const totalScore = document.getElementById("score")
+  totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+  if (currQuestion < Questions.length - 1) {
+      currQuestion++;
+      loadQues();
+  } else {
+      document.getElementById("opt").remove()
+      document.getElementById("ques").remove()
+      document.getElementById("btn").remove()
+      loadScore();
+  }
+}
+
+function checkAns() {
+  const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+  if (Questions[currQuestion].a[selectedAns].isCorrect) {
+      score++;
+      console.log("Correct")
+      nextQuestion();
+  } else {
+      nextQuestion();
+  }
+}
