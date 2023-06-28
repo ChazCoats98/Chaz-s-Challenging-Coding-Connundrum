c4220d2987873398ca0be6729eb8daacf8020d6a
Chaz Coats <chazcoats2@hotmail.com>
2023-06-28 15:55:57 -0400

    going a different direction

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,23 @@
-var button = document.getElementById("#startBtn");
-var quiz = document.getElementById("#quiz");
-var timerDisplay = document.getElementById("#timer");
-var questionEl = document.getElementById("#questions");
-var answerEl = document.getElementsByClassName("#answers");
-var answerA = document.getElementById("#answer-a");
-var answerB = document.getElementById("#answer-b");
-var answerC = document.getElementById("#answer-c");
-var answerD = document.getElementById("#answer-d");
-var scoreBox = document.getElementById("#score");
-var contBtn = document.getElementById("#contBtn");
+var button = document.getElementById("startBtn");
+var quizBox = document.getElementById("quizBox");
+var timerDisplay = document.getElementById("timer");
+var questionEl = document.getElementById("questions");
+var answerEl = document.getElementById("answerEl");
+var scoreBox = document.getElementById("score");
+var contBtn = document.getElementById("contBtn");
+var p = document.getElementById("h2Footer");
+var newLi = document.createElement("li");
+questionEl.innerText = "Javascript Quiz";
+p.innerText = "This is a timed quiz. You have 60 seconds to answer each question correctly. If you answer a question incorrectly 10 seconds will be deducted from the remaining time.";
 var initTime = 60;
 var score = 0;
 var questionNumber = 0;
 
-var questions = [
+var qanda = [
   {
     question: "What is the DOM?",
     answers: {
-      A: "",
+      A: "First Question",
       B: "",
       C: "",
       D: "",
@@ -66,30 +66,27 @@ var questions = [
   },
 ]
 
-function questionDisplay() {
-  deselector();
-questionEl.textContent = questions[questionNumber].question;
-answerEl.innerHTML= ""
-for (let i=0; i < questions[questionNumber].A.length; i++) {
-  var answersDiv = document.createElement("div");
-  var answerlist = document.createElement("input");
-  var answerLabel = document.createElement("label");
-
-  answerlist.type = "radio";
-  answerlist.name = "answer";
-  answerlist.value = i;
-
-  answerLabel.textContent = questions[questionNumber].A[i].text;
-
-  answersDiv.appendChild(answerlist);
-  answersDiv.appendChild(answerLabel);
-  answerEl.appendChild(answersDiv);
-  }
+function quizDisplay() {
+  questionEl.innerText = "" + firstQ.question;
+  p.style.display = "none";
+qanda.answers.array.forEach(answersLi => {
+  var newLi = document.createElement("li");
+  
+});
+  var aLi = document.createElement("li");
+  var bLi = document.createElement("li");
+  var cLi = document.createElement("li");
+  var dLi = document.createElement("li");
+  aLi.setAttribute("radio", "");
+  answerEl.appendChild(aLi);
+  answerEl.appendChild(bLi);
+  answerEl.appendChild(cLi);
+  answerEl.appendChild(dLi);
+  button.style.display = "none";
+  var newBtn = document.createElement("button");
+  newBtn.innerText = "Check Answer";
+  quizBox.appendChild(newBtn);
 }
-
-questionDisplay();
-
-
 function timer() {
   var timerVal = setInterval(function() {
   initTime--;
@@ -97,107 +94,8 @@ function timer() {
    }, 1000);
 }
 
-button.addEventListener("click", function() {
-  button.style.display = "none";
-  contBtn.style.display = "inline";
-  const answer = userAnswer();
-  if (answer) {
-    if (answer === questions[questionNumber].rightanswer) score++;
-      questions++;
-    
-    if (questionNumber < questions.length) questionDisplay();
-      else {
-        quiz.innerHTML =
-      }
-    
-    
-  }
-})
-const Questions = [{
-  q: "What is capital of India?",
-  a: [{ text: "Gandhinagar", isCorrect: false },
-  { text: "Surat", isCorrect: false },
-  { text: "Delhi", isCorrect: true },
-  { text: "Mumbai", isCorrect: false }
-  ]
-
-},
-{
-  q: "What is the capital of Thailand?",
-  a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-  { text: "Phuket", isCorrect: false },
-  { text: "Ayutthaya", isCorrect: false },
-  { text: "Bangkok", isCorrect: true }
-  ]
-
-},
-{
-  q: "What is the capital of Gujarat",
-  a: [{ text: "Surat", isCorrect: false },
-  { text: "Vadodara", isCorrect: false },
-  { text: "Gandhinagar", isCorrect: true },
-  { text: "Rajkot", isCorrect: false }
-  ]
-
-}
-
-]
-
-let currQuestion = 0
-let score = 0
-
-function loadQues() {
-  const question = document.getElementById("ques")
-  const opt = document.getElementById("opt")
-
-  question.textContent = Questions[currQuestion].q;
-  opt.innerHTML = ""
-
-  for (let i = 0; i < Questions[currQuestion].a.length; i++) {
-      const choicesdiv = document.createElement("div");
-      const choice = document.createElement("input");
-      const choiceLabel = document.createElement("label");
-
-      choice.type = "radio";
-      choice.name = "answer";
-      choice.value = i;
-
-      choiceLabel.textContent = Questions[currQuestion].a[i].text;
-
-      choicesdiv.appendChild(choice);
-      choicesdiv.appendChild(choiceLabel);
-      opt.appendChild(choicesdiv);
-  }
-}
-
-loadQues();
-
-function loadScore() {
-  const totalScore = document.getElementById("score")
-  totalScore.textContent = `You scored ${score} out of ${Questions.length}`
-}
 
 
-function nextQuestion() {
-  if (currQuestion < Questions.length - 1) {
-      currQuestion++;
-      loadQues();
-  } else {
-      document.getElementById("opt").remove()
-      document.getElementById("ques").remove()
-      document.getElementById("btn").remove()
-      loadScore();
-  }
-}
-
-function checkAns() {
-  const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
-
-  if (Questions[currQuestion].a[selectedAns].isCorrect) {
-      score++;
-      console.log("Correct")
-      nextQuestion();
-  } else {
-      nextQuestion();
-  }
-}
+button.addEventListener("click", function transition() {
+quizDisplay();
+});
